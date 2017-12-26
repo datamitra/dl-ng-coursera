d213f6c0b7c8a4e6e2a5ef74909e7f2c12960179
smayakkaruppan <shivam.ms@gmail.com>
2017-12-26 08:00:35 -0500

    course work

---
+++ practice.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+a = np.random.randn(2, 3) # a.shape = (2, 3)
+b = np.random.randn(2, 1) # b.shape = (2, 1)
+c = a + b
+
+print(c)
+
+"""
+a = np.random.randn(4, 3) # a.shape = (4, 3)
+b = np.random.randn(3, 2) # b.shape = (3, 2)
+c = a*b
+"""
+
+print(c)
+
+a = np.random.randn(12288, 150) # a.shape = (12288, 150)
+b = np.random.randn(150, 45) # b.shape = (150, 45)
+c = np.dot(a,b)
+print(c.shape)
+
+a = np.random.randn(3, 3)
+b = np.random.randn(3, 1)
+c = a*b
+
+print (c.shape)
